36ce02591798848e52a558445f3fee31d3324b18
winterandrea <andrecasero@gmail.com>
2020-08-21 16:35:08 +0200

    high hero resolution

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { StaticQuery, graphql } from "gatsby";
-import Img from "gatsby-image";
+import Img from "gatsby-image/withIEPolyfill";
 
 const Image = ({ fileName, alt, objectPosition, objectFit, ...restProps }) => (
   <StaticQuery
